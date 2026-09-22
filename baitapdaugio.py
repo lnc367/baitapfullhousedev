15f3e1fa189d38c315cb0a02cd7756a5ef216426
lnc367 <charlie.le2907@gmail.com>
2026-09-22 20:11:01 +0700

    Add files via upload

---
+++ baitapdaugio.py
@@ -0,0 +1,16 @@
+def print_menu():
+    print("="*40)
+    print("   QUẢN LÝ KHO HÀNG - GROCERY STORE")
+    print("="*40)
+    print("""
+          1. Xem danh sách tồn kho
+          2. Nhập thêm hàng hóa mới
+          3. Cập nhật số lượng tồn kho theo ID
+          4. Thoát chương trình
+          """)
+    print("="*40)
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
